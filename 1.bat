c2d74d5165945848698c7da34138688fc9524c68
LYQWE <LYQWE@github.com>
2018-02-26 02:20:17 +0800

    LYQWE

--- 1.bat
+++ 1.bat
@@ -5,7 +5,14 @@ cd /d %cd%
 git init
 git config --local user.name "%name%"
 git config --local user.email "%name%@github.com"
+
+
 git add .
+git reset HEAD 1.bat
+git checkout -- 1.bat
+git status
+
+
 git commit -m "%name%"
 git remote add origin git@%name%:%name%/%name%.github.io.git
 git pull --rebase origin master
